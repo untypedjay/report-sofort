73eb3bd1feb68f807bcdb8dfad35aef1eda3d927
Jay Dee <jay.developes@simplelogin.fr>
2021-03-11 12:41:52 +0100

    Add Comment

---
+++ src/components/Comment/Comment.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaComment } from 'react-icons/fa';
+
+interface Props {
+  timestamp: string,
+  children: string
+}
+
+export default function Comment({ timestamp, children }: Props) {
+  return (
+    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+      <div className="w-0 flex-1 flex items-center">
+        <FaComment/>
+        <span className="ml-2 flex-1 w-0 truncate">{ children }</span>
+        <span>{ timestamp }</span>
+      </div>
+    </li>
+  );
+}
